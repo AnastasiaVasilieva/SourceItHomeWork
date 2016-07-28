c10a244c2e4dd5bda8259fe6931448b4eecc21b7
Anastasia <vasilieva.anastasia20@gmail.com>
2016-07-28 03:07:59 +0300

    ex

---
+++ examTask/src/impl/lottery/Service.java
@@ -0,0 +1,70 @@
+package impl.lottery;
+
+import api.lottery.Lottery;
+import api.lottery.Lottery.Type;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Nastya on 27.07.2016.
+ */
+public class Service implements api.lottery.Service {
+    Map<Long, Lottery> lotteries = new HashMap<>();
+    List<api.lottery.Ticket> winningTickets;
+    /**
+     * should create new lottery with certain type and put it into map.
+     * Also this method should create lottery tickets and put them into lottery.
+     *
+     * @param type
+     * @return number of ticket
+     */
+    @Override
+    public Long performNewLottery(Type type) {
+        switch (type) {
+            case LOTTO:
+                winningTickets = LotoLottery.winnigTickets();
+                LotoLottery lotoLottery = new LotoLottery();
+                for (long i = 0; i < 1000000; i++)
+                    lotoLottery.tickets.add(new Ticket(i));
+                lotteries.put(lotoLottery.getLotteryNumber(), lotoLottery);
+                lotoLottery.performLottery();
+
+                return lotoLottery.getLotteryNumber();
+            case INSTANT:
+                winningTickets = InstantLottery.winnigTickets();
+                InstantLottery instant = new InstantLottery();
+                instant.performLottery();
+                for (long i = 0; i < 1000000; i++) {
+                    instant.tickets.add(new Ticket(i));
+                }
+                lotteries.put(instant.getLotteryNumber(), instant);
+                return instant.getLotteryNumber();
+        }
+        return null;
+    }
+
+    @Override
+    public List<api.lottery.Ticket> getLotteryTickets(Long lotteryNumber) {
+
+        return lotteries.get(lotteryNumber).getLotteryTickets();
+    }
+
+    @Override
+    public Double takeMoney(Long lotteryNumber, api.lottery.Ticket ticket) {
+
+        if (winningTickets.contains(ticket)) {
+            return lotteries.get(lotteryNumber).takeMoney(ticket);
+        } else return null;
+    }
+
+    @Override
+    public void endLottery(Long lotteryNumber) {
+        lotteries.remove(lotteryNumber);
+
+    }
+
+
+}
